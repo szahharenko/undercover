1262243c2be4d92c70bfeaa264304b5d861a82a0
Sergei <sergei.zahharenko@riverty.com>
2026-01-28 11:04:25 +0200

    Updates

--- src/components/EventsCalendar.tsx
+++ src/components/EventsCalendar.tsx
@@ -5,9 +5,49 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list'
 import rrulePlugin from '@fullcalendar/rrule'
 import FullCalendar from '@fullcalendar/react';
+import dune from '../assets/dune-cover.jpg';
+import kanban from '../assets/kanban-cover.jpg';
+import duneExp from '../assets/dune-bloodlines-cover.jpg';
 
 const EventsCalendar: React.FC = () => {
   const { t, i18n } = useTranslation();
+
+  const events = [
+    {
+      title: `${t('events_calendar.game_evening')}`,
+      date: '2026-02-05',
+      rrule: {
+        freq: 'weekly',
+        byweekday: ['we'],
+        dtstart: '2026-02-05T18:00:00'
+      },
+      duration: '05:00',
+      extendedProps: {
+        description: `
+          <img src="${dune}" alt="Dune: Uprising" style="height: 8rem;margin-right: 1rem;margin-bottom: 0.5rem;" class="float-left"/>
+          <img src="${duneExp}" alt="Dune: Bloodlines" style="height: 8rem;margin-right: 1rem;margin-bottom: 0.5rem;" class="float-left"/>
+          <img src="${kanban}" alt="Kanban" style="height: 8rem;margin-right: 1rem;margin-bottom: 0.5rem;" class="float-left"/>
+          Dune: Uprising wtih Bloodlines, Kanban, Marco Polo, and other games...<br/>
+          ☕ Hot drinks included<br/>
+          🍪🥐🥨 Snacks provided<br/>
+          📍 Kivimurru tn 34 - 6, 11411 Tallinn<br/>
+        `
+      }
+    },
+
+    {
+      title: `${t('events_calendar.game_weekend')}`,
+      date: '2026-02-05',
+      startTime: '12:00',
+      endTime: '22:00',
+      rrule: {
+        freq: 'weekly',
+        byweekday: ['sa', 'su'],
+        dtstart: '2026-02-05T12:00:00'
+      },
+      duration: '09:00'
+    },
+  ];
 
   return (
     <section className="container mx-auto py-12">
@@ -16,31 +56,28 @@ const EventsCalendar: React.FC = () => {
         plugins={[listPlugin, timeGridPlugin, dayGridPlugin, rrulePlugin]}
         initialView="listMonth"
         locale={i18n.language}
-        events={[
-          {
-            title: `18:00 - 22:00 ${t('events_calendar.game_evening')}`,
-            date: '2026-02-05',
-            rrule: {
-              freq: 'weekly',
-              byweekday: ['we'],
-              dtstart: '2026-02-05T18:00:00'
-            },
-            duration: '05:00'
-          },
-
-          {
-            title: `12:00 - 21:00 ${t('events_calendar.game_weekend')}`,
-            date: '2026-02-05',
-            startTime: '12:00',
-            endTime: '22:00',
-            rrule: {
-              freq: 'weekly',
-              byweekday: ['sa', 'su'],
-              dtstart: '2026-02-05T12:00:00'
-            },
-            duration: '09:00'
-          },
-        ]}
+        eventContent={(arg) => (
+          <div>
+            <div className="fc-event-title"><strong>{arg.event.title}</strong></div>
+            <div
+              className="fc-event-desc"
+              dangerouslySetInnerHTML={{
+                __html: arg.event.extendedProps.description
+              }}
+            />
+          </div>
+        )}
+        slotLabelFormat={{
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false
+        }}
+        eventTimeFormat={{
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false
+        }}
+        events={events}
       />
     </section>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer: React.FC = () => {
       whileInView={{ opacity: 1 }}
       viewport={{ once: true, amount: 0.3 }}
       transition={{ duration: 0.8 }}
-      className="bg-charcoal text-cream p-20"
+      className="bg-charcoal text-cream p-10 py-20"
     >
       <div className="container mx-auto px-4 text-center">
         <div className="">
@@ -26,10 +26,7 @@ const Footer: React.FC = () => {
             <div className='flex flex-col md:flex-row w-full items-center'>
               <div className='flex text-center flex-col p-2 w-full md:w-[50%]'>
                 <h3 className="text-xl font-bold mb-4">{t('footer.social_title')}</h3>
-                <div className="flex justify-center space-x-6">
-                  <a href="https://www.facebook.com/undercovervibe" className="hover:text-sage-green transition-colors" aria-label="Facebook">
-                    <FaFacebook size={28} />
-                  </a>
+                <div className="flex justify-center p-4 space-x-6">
                   <a href="https://www.instagram.com/undercovertallinn" className="hover:text-sage-green transition-colors" aria-label="Instagram">
                     <FaInstagram size={28} />
                   </a>
@@ -39,19 +36,16 @@ const Footer: React.FC = () => {
                   <a href='https://wa.me/3725154369' className="hover:text-sage-green transition-colors" aria-label="WhatsApp">
                     <FaWhatsapp size={28} />
                   </a>
-                  <a href="tel:+3725154369" className="hover:text-sage-green transition-colors" aria-label="Phone">
-                    <FaPhone size={28} />
-                  </a>
-                  <a href="mailto:info@undercover.ee" className="hover:text-sage-green transition-colors" aria-label="Email">
-                    <MdMailOutline size={28} />
+                  <a href="https://www.facebook.com/undercovervibe" className="hover:text-sage-green transition-colors" aria-label="Facebook">
+                    <FaFacebook size={28} />
                   </a>
                 </div>
               </div>
               <div className='flex flex-col w-full md:w-[50%]'>
                 <h3 className="text-xl font-bold mb-4">{t('footer.location_title')}</h3>
                 <p><a href="https://maps.app.goo.gl/z4kknp2yiiwf39QJ8">Kivimurru tn 34 - 6, 11411 Tallinn</a></p>
-                <p>Email: <a href="mailto:info@undercover.ee">info@undercover.ee</a></p>
-                <p>Phone: <a href="tel:+3725154369">(+372) 51 54 369</a></p>
+                <p><MdMailOutline style={{display: 'inline'}} size={16}  /> <a href="mailto:info@undercover.ee">info@undercover.ee</a></p>
+                <p><FaPhone style={{display: 'inline'}} size={16} /> <a href="tel:+3725154369">(+372) 51 54 369</a></p>
               </div>
             </div>
             <img src={dog} alt="Undercover vibe logo" className='max-h-[150px] h-auto'/>
